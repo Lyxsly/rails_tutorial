902b7c59981c0418c00d07e918c2a9685df79ccc
tokinari miyata <tokinari.miyata@kufu.co.jp>
2026-06-09 18:21:27 +0900

    11.21まで

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   get "/help", to:"static_pages#help"
   get "/about", to:"static_pages#about"
   get "/contact", to:"static_pages#contact"
-  
-  # get "/users", to:"users#index"
-  
   get "/signup",to:"users#new"
-
   get "/login",to:"sessions#new"
   post "/login",to:"sessions#create"
   delete "/logout",  to: "sessions#destroy"
   resources :users
+  resources :account_activations,only:[:edit]
 end
